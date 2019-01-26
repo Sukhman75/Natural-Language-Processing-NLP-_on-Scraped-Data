c5e96281c0def03aa18834133f5e4d1aee4e8d9c
Sukhman75 <sukhmansingh75@gmail.com>
2019-01-26 11:55:03 -0500

    Scraped Projects

--- Project_Code.py
+++ Project_Code.py
@@ -1,5 +1,27 @@
 import requests
 from bs4 import BeautifulSoup 
-
+from random import choice
 Url1 = "http://quotes.toscrape.com/"
 
+
+def scrap_quotes():
+ url2 = "/page/1/"
+ all_quotes = []
+ while url2:
+  res = requests.get(f"{Url1}{url2}")
+  print(f"Scraping {Url1}{url2}")
+  soup = BeautifulSoup(res.text, "html.parser")
+  quotes = soup.find_all(class_="quote")
+  
+  for quote in quotes:
+   all_quotes.append({"text": quote.find(class_="text").get_text()})
+  next_button = soup.find(class_="next")
+  url2 = next_button.find("a")["href"] if next_button else None
+ return all_quotes
+
+def start_game(scrapQuotes):
+ quote = choice(scrapQuotes)
+ Text_data = quote['text']
+ print(Text_data)
+scrapQuotes = scrap_quotes()
+start_game(scrapQuotes)
